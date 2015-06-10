94678a7dd3cf66fa6e45f0f39b3ccfc1e06c8eeb
Hodong Kim <cogniti@gmail.com>
2015-06-10 22:44:06 +0900

    Added dasom-key-syms.h

---
+++ libdasom/dasom-key-syms.h
@@ -0,0 +1,83 @@
+/* -*- Mode: C; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-  */
+/*
+ * dasom-key-syms.h
+ * This file is part of Dasom.
+ *
+ * Copyright (C) 2015 Hodong Kim <hodong@cogno.org>
+ *
+ * Dasom is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Dasom is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program;  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __DASOM_KEY_SYMS_H__
+#define __DASOM_KEY_SYMS_H__
+
+#define DASOM_KEY_space         0x0020
+
+#define DASOM_KEY_0             0x030
+#define DASOM_KEY_1             0x031
+#define DASOM_KEY_2             0x032
+#define DASOM_KEY_3             0x033
+#define DASOM_KEY_4             0x034
+#define DASOM_KEY_5             0x035
+#define DASOM_KEY_6             0x036
+#define DASOM_KEY_7             0x037
+#define DASOM_KEY_8             0x038
+#define DASOM_KEY_9             0x039
+
+#define DASOM_KEY_h             0x068
+
+#define DASOM_KEY_j             0x06a
+#define DASOM_KEY_k             0x06b
+#define DASOM_KEY_l             0x06c
+
+#define DASOM_KEY_BackSpace     0xff08
+#define DASOM_KEY_Return        0xff0d
+
+#define DASOM_KEY_Escape        0xff1b
+
+#define DASOM_KEY_Hangul        0xff31
+
+#define DASOM_KEY_Hangul_Hanja  0xff34
+
+#define DASOM_KEY_Left          0xff51
+#define DASOM_KEY_Up            0xff52
+#define DASOM_KEY_Right         0xff53
+#define DASOM_KEY_Down          0xff54
+#define DASOM_KEY_Page_Up       0xff55
+#define DASOM_KEY_Page_Down     0xff56
+
+#define DASOM_KEY_KP_Enter      0xff8d
+
+#define DASOM_KEY_KP_Left       0xff96
+#define DASOM_KEY_KP_Up         0xff97
+#define DASOM_KEY_KP_Right      0xff98
+#define DASOM_KEY_KP_Down       0xff99
+#define DASOM_KEY_KP_Page_Up    0xff9a
+#define DASOM_KEY_KP_Page_Down  0xff9b
+
+#define DASOM_KEY_KP_0          0xffb0
+#define DASOM_KEY_KP_1          0xffb1
+#define DASOM_KEY_KP_2          0xffb2
+#define DASOM_KEY_KP_3          0xffb3
+#define DASOM_KEY_KP_4          0xffb4
+#define DASOM_KEY_KP_5          0xffb5
+#define DASOM_KEY_KP_6          0xffb6
+#define DASOM_KEY_KP_7          0xffb7
+#define DASOM_KEY_KP_8          0xffb8
+#define DASOM_KEY_KP_9          0xffb9
+
+#define DASOM_KEY_Shift_L       0xffe1
+#define DASOM_KEY_Shift_R       0xffe2
+
+#endif /* __DASOM_KEY_SYMS_H__ */
