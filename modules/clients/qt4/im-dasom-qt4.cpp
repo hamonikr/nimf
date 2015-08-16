8935728614ae99fc28fb77f1dea0ea1fc71fb3fb
Hodong Kim <cogniti@gmail.com>
2015-08-17 01:58:40 +0900

    Added copyright notice

--- modules/clients/qt4/im-dasom-qt4.cpp
+++ modules/clients/qt4/im-dasom-qt4.cpp
@@ -1,4 +1,23 @@
 /* -*- Mode: C++; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*- */
+/*
+ * im-dasom-qt4.cpp
+ * This file is part of Dasom.
+ *
+ * Copyright (C) 2015 Hodong Kim <hodong@cogno.org>
+ *
+ * Dasom is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Dasom is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program;  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <QTextFormat>
 #include <QInputContext>
