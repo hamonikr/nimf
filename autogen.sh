605922722ecfff3644c7111474fc806d8a386c2a
Hodong Kim <cogniti@gmail.com>
2016-04-30 22:39:25 +0900

    Check pkg-config, libglib2.0-dev
    Related issue https://github.com/dasom-im/dasom-jeongeum/issues/9

--- autogen.sh
+++ autogen.sh
@@ -17,6 +17,18 @@ else
     intltoolize --force --copy --automake || exit $?
 fi
 
+PKGCONFIG=`which pkg-config`
+if test -z "$PKGCONFIG"; then
+    echo "pkg-config not found, please install pkg-config"
+    exit 1
+fi
+
+pkg-config --print-errors glib-2.0
+if [ $? != 0 ]; then
+    echo "You probably need to install 'libglib2.0-dev'"
+    exit 1
+fi
+
 AUTORECONF=`which autoreconf`
 if test -z $AUTORECONF; then
     echo "autoreconf not found, please install autoconf package"
