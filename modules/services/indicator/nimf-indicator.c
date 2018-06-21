f4566306cfb84a0abe105520bebe2ce5bb48a7e2
Hodong Kim <cogniti@gmail.com>
2018-06-21 12:05:18 +0900

    Changed project site to https://gitlab.com/hodong/nimf

--- modules/services/indicator/nimf-indicator.c
+++ modules/services/indicator/nimf-indicator.c
@@ -106,7 +106,7 @@ on_menu_about (GSimpleAction *action,
     "program-name",       _("Nimf"),
     "translator-credits", _("Hodong Kim, N"),
     "version",            VERSION,
-    "website",            "https://cogniti-works.blogspot.com/search/label/nimf",
+    "website",            "https://gitlab.com/hodong/nimf",
     "website-label",      _("Website"),
     NULL);
 
