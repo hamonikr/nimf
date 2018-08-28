2a7be3aa408de76be05ffe8f19f2ed7b400df4f1
Hodong Kim <cogniti@gmail.com>
2018-08-28 23:14:57 +0900

    법적 고지 및 면책 조항 추가
    커뮤니티의 위력을 이용하여 개발자의 시간과 노동력을 약탈하려는 행위가 프로젝트 초기부터 지금까지 지속되고 있습니다.
    과거에는 단순히 음해하는 수준이었지만 현재는 허위 사실을 유포함으로써 명예를 훼손하는 수준에 이르렀습니다.
    사람들이 이슈를 작성하면 제가 스트레스를 받는 이유가 이런 이유 때문입니다.
    요구를 거부하거나 신속히 해결해 드리지 않으면 자신이 속한 커뮤니티에 저를 음해하고 허위 사실을 유포하는 일이 발생하기 때문에 법적 고지와 면책 조항을 한글(한국어)로 추가합니다.

--- modules/services/preedit-window/nimf-preedit-window.c
+++ modules/services/preedit-window/nimf-preedit-window.c
@@ -5,6 +5,27 @@
  *
  * Copyright (C) 2017,2018 Hodong Kim <cogniti@gmail.com>
  *
+ * # 법적 고지
+ *
+ * Nimf 소프트웨어는 대한민국 저작권법과 국제 조약의 보호를 받습니다.
+ * Nimf 개발자는 대한민국 법률의 보호를 받습니다.
+ * 커뮤니티의 위력을 이용하여 개발자의 시간과 노동력을 약탈하려는 행위를 금하시기 바랍니다.
+ *
+ * * 커뮤니티 게시판에 개발자를 욕(비난)하거나
+ * * 욕보이는(음해하는) 글을 작성하거나
+ * * 허위 사실을 공표하거나
+ * * 명예를 훼손하는
+ *
+ * 등의 행위는 정보통신망 이용촉진 및 정보보호 등에 관한 법률의 제재를 받습니다.
+ *
+ * # 면책 조항
+ *
+ * Nimf 는 무료로 배포되는 오픈소스 소프트웨어입니다.
+ * Nimf 개발자는 개발 및 유지보수에 대해 어떠한 의무도 없고 어떠한 책임도 없습니다.
+ * 어떠한 경우에도 보증하지 않습니다. 도덕적 보증 책임도 없고, 도의적 보증 책임도 없습니다.
+ * Nimf 개발자는 리브레오피스, 이클립스 등 귀하가 사용하시는 소프트웨어의 버그를 해결해야 할 의무가 없습니다.
+ * Nimf 개발자는 귀하가 사용하시는 배포판에 대해 기술 지원을 해드려야 할 의무가 없습니다.
+ *
  * Nimf is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
